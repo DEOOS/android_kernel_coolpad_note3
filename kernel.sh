2da4a89c830c5aefddfdcd8dbaf35b76b159ce8b
VirajKanse <virajv2k@gmail.com>
2017-08-08 14:46:01 +0530

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,4 @@
 PATH=${PATH}:${PWD}/toolchain/bin/
 export ARCH=arm64
-make CP8676_I02_defconfig ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
+make cp_n3_defconfig ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
 make -j30 ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
